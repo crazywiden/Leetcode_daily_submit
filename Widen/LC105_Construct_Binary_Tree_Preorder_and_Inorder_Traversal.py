2ccfc469869478d9af8cfc4fbde679b83f99952d
widen <yuangao719@gmail.com>
2019-10-01 20:32:57 -0500

    new method

--- Widen/LC105_Construct_Binary_Tree_Preorder_and_Inorder_Traversal.py
+++ Widen/LC105_Construct_Binary_Tree_Preorder_and_Inorder_Traversal.py
@@ -34,28 +34,28 @@ class TreeNode:
         self.left = None
         self.right = None
 
-class Solution:
-    def buildTree(self, preorder: List[int], inorder: List[int]) -> TreeNode:
-        n = len(preorder)
-        if n == 0:
-            return None
-        if n == 1:
-            return TreeNode(preorder[0])
+# class Solution:
+#     def buildTree(self, preorder: List[int], inorder: List[int]) -> TreeNode:
+#         n = len(preorder)
+#         if n == 0:
+#             return None
+#         if n == 1:
+#             return TreeNode(preorder[0])
         
-        root = TreeNode(preorder[0])
-        idx = inorder.index(preorder[0])
+#         root = TreeNode(preorder[0])
+#         idx = inorder.index(preorder[0])
         
-        left_node_preorder = preorder[1:idx+1]
-        left_node_inorder = inorder[:idx]
-        left_node = self.buildTree(left_node_preorder, left_node_inorder)
+#         left_node_preorder = preorder[1:idx+1]
+#         left_node_inorder = inorder[:idx]
+#         left_node = self.buildTree(left_node_preorder, left_node_inorder)
         
-        right_node_preorder = preorder[idx+1:]
-        right_node_inorder = inorder[idx+1:]
-        right_node = self.buildTree(right_node_preorder, right_node_inorder)
+#         right_node_preorder = preorder[idx+1:]
+#         right_node_inorder = inorder[idx+1:]
+#         right_node = self.buildTree(right_node_preorder, right_node_inorder)
         
-        root.left = left_node
-        root.right = right_node
-        return root
+#         root.left = left_node
+#         root.right = right_node
+#         return root
 
 
 
@@ -63,44 +63,52 @@ class Solution:
 # used two tricks:
 # 1. use hash map to speed up search efficiency
 # 2. use iter/next to simplify code
+# Runtime: 36 ms, faster than 99.98% of Python3 online submissions for Construct Binary Tree from Preorder and Inorder Traversal.
+# Memory Usage: 13.9 MB, less than 100.00% of Python3 online submissions for Construct Binary Tree from Preorder and Inorder Traversal.
 
-class Solution(object):
-    def buildTree(self, preorder, inorder):
-        l = len(inorder)
-        inor_dict = {i:idx for idx, i in enumerate(inorder)}
-        pre_iter = iter(preorder)
-        
-        def helper(start, end):
-            if start > end: 
-                return None
-            num = next(pre_iter)
-            root = TreeNode(num)
-            idx = inor_dict[num]
-            root.left = helper(start, idx)
-            root.right = helper(idx+1, end)
-        return helper(0, l-1)
-
-
-
-# method3 --  use dictionary to speed up, non-recursion version
 class Solution:
-    def buildTree(self, preorder: List[int], inorder: List[int]) -> TreeNode:
-        if len(preorder) == 0:
-            return None
-        root = TreeNode(0)
-        lo, hi = 0, len(preorder) - 1
-        nodes = [(root, lo, hi)]
-        lut = {inorder[i]: i for i in range(len(inorder))}
-        # use a look up table to get the index
-        # of an element in constant time.
-        for i in range(len(preorder)):
-            curr, lo, hi = nodes.pop()
-            curr.val = preorder[i]
-            mid = lut[curr.val]
-            if mid < hi:
-                curr.right = TreeNode(0)
-                nodes.append((curr.right, mid + 1, hi))
-            if lo < mid:
-                curr.left = TreeNode(0)
-                nodes.append((curr.left, lo, mid - 1))
-        return root
+    def buildTree(self, preorder, inorder):
+        idx = {i:idx for idx, i in enumerate(inorder)}
+        n = len(preorder)
+        p = 0
+        def helper(start, end):
+            nonlocal p
+            if start > end or p == n:
+                return None
+            root = TreeNode(preorder[p])
+            mid = idx[preorder[p]]
+            print(preorder[p])
+            p += 1
+            root.left = helper(start, mid-1)
+            root.right = helper(mid+1, end)
+            return root
+        return helper(0, n-1)
+
+sol = Solution()
+preorder = [3,9,20,15,7]
+inorder = [9,3,15,20,7]
+sol.buildTree(preorder, inorder)
+
+
+# # method3 --  use dictionary to speed up, non-recursion version
+# class Solution:
+#     def buildTree(self, preorder: List[int], inorder: List[int]) -> TreeNode:
+#         if len(preorder) == 0:
+#             return None
+#         root = TreeNode(0)
+#         lo, hi = 0, len(preorder) - 1
+#         nodes = [(root, lo, hi)]
+#         lut = {inorder[i]: i for i in range(len(inorder))}
+#         # use a look up table to get the index
+#         # of an element in constant time.
+#         for i in range(len(preorder)):
+#             curr, lo, hi = nodes.pop()
+#             curr.val = preorder[i]
+#             mid = lut[curr.val]
+#             if mid < hi:
+#                 curr.right = TreeNode(0)
+#                 nodes.append((curr.right, mid + 1, hi))
+#             if lo < mid:
+#                 curr.left = TreeNode(0)
+#                 nodes.append((curr.left, lo, mid - 1))
+#         return root
