d1a75b755d10b0d7191777bf77cc4bec5fc44667
widen <yuangao719@gmail.com>
2019-10-06 01:57:23 -0500

    LC368

--- Widen/LC368_Largest_Divisible_Subset.py
+++ Widen/LC368_Largest_Divisible_Subset.py
@@ -17,64 +17,68 @@ Output: [1,2,4,8]
 """
 # method 1 -- dfs 
 # TLE at 31/41
-class Solution:
-    def largestDivisibleSubset(self, nums: List[int]) -> List[int]:
-        def dfs(nums, res_list, start):
-            if start >= len(nums)-1:
-                return res_list
-            longest_part = []
-            cnt = 0
-            for i in range(start+1, len(nums)):
-                if nums[i] % res_list[-1] == 0:
-                    res_list.append(nums[i])
-                    part_list = dfs(nums, res_list, i)
-                    if len(part_list) > cnt:
-                        cnt = len(part_list)
-                        longest_part = part_list.copy()
-                    res_list.pop()
-            if len(longest_part) == 0:
-                return res_list
-            else:
-                return longest_part
+# class Solution:
+#     def largestDivisibleSubset(self, nums: List[int]) -> List[int]:
+#         def dfs(nums, res_list, start):
+#             if start >= len(nums)-1:
+#                 return res_list
+#             longest_part = []
+#             cnt = 0
+#             for i in range(start+1, len(nums)):
+#                 if nums[i] % res_list[-1] == 0:
+#                     res_list.append(nums[i])
+#                     part_list = dfs(nums, res_list, i)
+#                     if len(part_list) > cnt:
+#                         cnt = len(part_list)
+#                         longest_part = part_list.copy()
+#                     res_list.pop()
+#             if len(longest_part) == 0:
+#                 return res_list
+#             else:
+#                 return longest_part
         
-        cnt = 0
-        longest_res = []
-        nums = sorted(nums)
-        search_history = set()
-        for i in range(len(nums)):
-            if i in search_history:
-                continue
-            tmp_res = dfs(nums, [nums[i]], i)
-            if len(tmp_res) > cnt:
-                longest_res = tmp_res.copy()
-                cnt = len(tmp_res)
-            search_history.update(tmp_res)
-        return longest_res
+#         cnt = 0
+#         longest_res = []
+#         nums = sorted(nums)
+#         search_history = set()
+#         for i in range(len(nums)):
+#             if i in search_history:
+#                 continue
+#             tmp_res = dfs(nums, [nums[i]], i)
+#             if len(tmp_res) > cnt:
+#                 longest_res = tmp_res.copy()
+#                 cnt = len(tmp_res)
+#             search_history.update(tmp_res)
+#         return longest_res
 
 
 # method 2 -- reference: https://blog.csdn.net/fuxuemingzhu/article/details/83027364
 # Runtime: 448 ms, faster than 22.02% of Python3 online submissions for Largest Divisible Subset.
 # Memory Usage: 13.9 MB, less than 20.00% of Python3 online submissions for Largest Divisible Subset.
-class Solution:
-    def largestDivisibleSubset(self, nums: List[int]) -> List[int]:
-        if not nums: 
-            return []
-        N = len(nums)
-        nums.sort()
-        dp = [0] * N # LDS
-        parent = [0] * N
-        mx = 0
-        mx_index = -1
-        for i in range(N):
-            for j in range(i - 1, -1 , -1):
-                if nums[i] % nums[j] == 0 and dp[i] < dp[j] + 1:
-                    dp[i] = dp[j] + 1
-                    parent[i] = j
-                    if dp[i] > mx:
-                        mx = dp[i]
-                        mx_index = i
-        res = list()
-        for k in range(mx + 1):
-            res.append(nums[mx_index])
-            mx_index = parent[mx_index]
-        return res[::-1]
+# class Solution:
+#     def largestDivisibleSubset(self, nums: List[int]) -> List[int]:
+#         if not nums: 
+#             return []
+#         N = len(nums)
+#         nums.sort()
+#         dp = [0] * N # LDS
+#         parent = [0] * N
+#         mx = 0
+#         mx_index = -1
+#         for i in range(N):
+#             for j in range(i - 1, -1 , -1):
+#                 if nums[i] % nums[j] == 0 and dp[i] < dp[j] + 1:
+#                     dp[i] = dp[j] + 1
+#                     parent[i] = j
+#                     if dp[i] > mx:
+#                         mx = dp[i]
+#                         mx_index = i
+#         res = list()
+#         for k in range(mx + 1):
+#             res.append(nums[mx_index])
+#             mx_index = parent[mx_index]
+#         return res[::-1]
+
+
+
+
