260c372561162602af6f3a86a1fd1f5fedb23df3
widen <yuangao719@gmail.com>
2019-10-07 04:46:04 -0500

    LC377

---
+++ Widen/LC377_Combination_Sum_IV.py
@@ -0,0 +1,36 @@
+"""
+Given an integer array with all positive numbers and no duplicates, find the number of possible combinations that add up to a positive integer target.
+
+Example:
+
+nums = [1, 2, 3]
+target = 4
+
+The possible combination ways are:
+(1, 1, 1, 1)
+(1, 1, 2)
+(1, 2, 1)
+(1, 3)
+(2, 1, 1)
+(2, 2)
+(3, 1)
+
+Note that different sequences are counted as different combinations.
+
+Therefore the output is 7.
+"""
+
+# classical dp, relatively easy
+# time complexity -- O(N^2)
+# space complexity -- O(1)
+# Runtime: 60 ms, faster than 18.51% of Python3 online submissions for Combination Sum IV.
+# Memory Usage: 14.1 MB, less than 22.22% of Python3 online submissions for Combination Sum IV.
+class Solution:
+    def combinationSum4(self, nums: List[int], target: int) -> int:
+        dp = [0] * (target + 1)
+        dp[0] = 1
+        for i in range(1, target + 1):
+            for num in nums:
+                if i >= num:
+                    dp[i] += dp[i - num]
+        return dp.pop()
