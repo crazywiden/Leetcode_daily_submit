c54198f8e076b69c2463067571408ec63faa4297
widen <yuangao719@gmail.com>
2019-10-26 12:10:49 -0500

    power solution

--- Widen/LC317_Shortest_Distance_from_All_Buildings.py
+++ Widen/LC317_Shortest_Distance_from_All_Buildings.py
@@ -110,4 +110,61 @@ class Solution(object):
         if res == 100000:
         	return -1
         return res
+
+
+# a powerful solution also using bfs
+# Runtime: 84 ms, faster than 81.91% of Python online submissions for Shortest Distance from All Buildings.
+# Memory Usage: 11.7 MB, less than 100.00% of Python online submissions for Shortest Distance from All Buildings.
+'''
+Use hit to record how many times a 0 grid has been reached and use distSum to record the sum of distance from all 1 grids to this 0 grid. A powerful pruning is that during the BFS we use count1 to count how many 1 grids we reached. If count1 < buildings then we know not all 1 grids are connected are we can return -1 immediately, which greatly improved speed (beat 100% submissions).
+'''
+
+class Solution(object):
+    def shortestDistance(self, grid):
+        """
+        :type grid: List[List[int]]
+        :rtype: int
+        """
+        if not grid or not grid[0]:
+            return -1
+        n, m = len(grid), len(grid[0])
+        buildings = 0
+        for i in range(n):
+            for j in range(m):
+                if grid[i][j] == 1:
+                    buildings += 1
+        hit = [[0]*m for _ in range(n)]
+        disSum = [[0]*m for _ in range(n)]
+
+        def bfs(start_x, start_y):
+            visited = [[False]*m for _ in range(n)]
+            visited[start_x][start_y] = True
+            q = collections.deque([(start_x, start_y, 0)])
+            # make sure every building can reach other buildings
+            count1 = 1
+            while q:
+                x, y, dist = q.popleft()
+                for nx, ny in ((x+1, y), (x-1, y), (x, y+1), (x, y-1)):
+                    if 0 <= nx < n  and 0 <= ny < m and not visited[nx][ny]:
+                        visited[nx][ny] = True
+                        if not grid[nx][ny]:
+                            q.append((nx, ny, dist + 1))
+                            disSum[nx][ny] += dist+1
+                            hit[nx][ny] += 1
+                        if grid[nx][ny] == 1:
+                            count1 += 1
+            return count1 == buildings
+
+        for i in range(n):
+            for j in range(m):
+                # BFS on each building, check if all buildings can be reached as well as updating each 0 with the steps it took to get there.
+                if grid[i][j] == 1:
+                    if not bfs(i, j):
+                        return -1
+        min_dist = float('inf')
+        for i in range(n):
+            for j in range(m):
+                if hit[i][j] == buildings:
+                    min_dist = min(min_dist, disSum[i][j])
+        return -1 if min_dist == float('inf') else min_dist        
             
