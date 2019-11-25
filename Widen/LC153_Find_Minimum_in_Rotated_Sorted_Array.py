19942972dec934d2605a76d7df4e81857fed4357
widen <yuangao719@gmail.com>
2019-11-25 17:25:20 -0600

    LC153

---
+++ Widen/LC153_Find_Minimum_in_Rotated_Sorted_Array.py
@@ -0,0 +1,84 @@
+
+"""
+153. Find Minimum in Rotated Sorted Array
+Suppose an array sorted in ascending order is rotated at some pivot unknown to you beforehand.
+
+(i.e.,  [0,1,2,4,5,6,7] might become  [4,5,6,7,0,1,2]).
+
+Find the minimum element.
+
+You may assume no duplicate exists in the array.
+
+Example 1:
+
+Input: [3,4,5,1,2] 
+Output: 1
+Example 2:
+
+Input: [4,5,6,7,0,1,2]
+Output: 0
+"""
+
+# binary search
+# time complexity -- O(logN)
+# Runtime: 44 ms, faster than 80.44% of Python3 online submissions for Find Minimum in Rotated Sorted Array.
+# Memory Usage: 12.9 MB, less than 100.00% of Python3 online submissions for Find Minimum in Rotated Sorted Array.
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+        if nums[0] < nums[-1]:
+            return nums[0]
+        if len(nums) == 1:
+            return nums[0]
+        left, right = 0, len(nums) - 1
+        while left < right:
+            mid = (left + right) // 2
+            if nums[mid] > nums[right]:
+                left = mid + 1
+            else:
+                right = mid
+                
+        return nums[left]
+
+
+
+# better solution
+# every time compare with nums[0], not with nums[right]
+class Solution(object):
+    def findMin(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        # If the list has just one element then return that element.
+        if len(nums) == 1:
+            return nums[0]
+
+        # left pointer
+        left, right = 0, len(nums) - 1
+
+        # if the last element is greater than the first element then there is no rotation.
+        # e.g. 1 < 2 < 3 < 4 < 5 < 7. Already sorted array.
+        # Hence the smallest element is first element. A[0]
+        if nums[right] > nums[0]:
+            return nums[0]
+
+        # Binary search way
+        while right >= left:
+            # Find the mid element
+            mid = int(left + (right - left) / 2)
+            # if the mid element is greater than its next element then mid+1 element is the smallest
+            # This point would be the point of change. From higher to lower value.
+            if nums[mid] > nums[mid + 1]:
+                return nums[mid + 1]
+            # if the mid element is lesser than its previous element then mid element is the smallest
+            if nums[mid - 1] > nums[mid]:
+                return nums[mid]
+
+            # if the mid elements value is greater than the 0th element this means
+            # the least value is still somewhere to the right as we are still dealing with elements greater than nums[0]
+            if nums[mid] > nums[0]:
+                left = mid + 1
+            # if nums[0] is greater than the mid value then this means the smallest value is somewhere to the left
+            else:
+                right = mid - 1
+    
