972c1e367e9ecce3477f05d182bb16c24e13ef73
widen <yuangao719@gmail.com>
2019-11-25 22:26:30 -0600

    LC236

---
+++ Widen/LC236_Lowest_Common_Ancestor_of_a_Binary_Tree.py
@@ -0,0 +1,42 @@
+"""
+236. Lowest Common Ancestor of a Binary Tree
+Given a binary tree, find the lowest common ancestor (LCA) of two given nodes in the tree.
+
+According to the definition of LCA on Wikipedia: “The lowest common ancestor is defined between two nodes p and q as the lowest node in T that has both p and q as descendants (where we allow a node to be a descendant of itself).”
+
+Given the following binary tree:  root = [3,5,1,6,2,0,8,null,null,7,4]
+"""
+
+
+
+
+# divide and conquer
+# if a node's left has q and right has p, then that's the result we want
+# Runtime: 60 ms, faster than 99.87% of Python3 online submissions for Lowest Common Ancestor of a Binary Tree.
+# Memory Usage: 22.9 MB, less than 91.67% of Python3 online submissions for Lowest Common Ancestor of a Binary Tree.
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        if not root:
+            return None
+        if root is p or root is q:
+            return root
+        
+        left = self.lowestCommonAncestor(root.left, p, q)
+        right = self.lowestCommonAncestor(root.right, p, q)
+        
+        if left is not None and right is not None:
+            return root
+        if left is not None and right is None:
+            return left
+        if left is None and right is not None:
+            return right
+        return None 
+        
