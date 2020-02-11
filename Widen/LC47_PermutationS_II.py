60ada35eb4eeb45f9d28076c4a9fdcc301b31b79
widen <yuangao719@gmail.com>
2020-02-11 11:56:19 -0500

    LC47

---
+++ Widen/LC47_PermutationS_II.py
@@ -0,0 +1,43 @@
+"""
+LC47 permutations
+Given a collection of numbers that might contain duplicates, return all possible unique permutations.
+
+Example:
+
+Input: [1,1,2]
+Output:
+[
+  [1,1,2],
+  [1,2,1],
+  [2,1,1]
+]
+"""
+
+
+# Runtime: 48 ms, faster than 93.42% of Python3 online submissions for Permutations II.
+# Memory Usage: 12.8 MB, less than 100.00% of Python3 online submissions for Permutations II.
+class Solution:
+    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        visited = set([])
+        self.dfs(res, [], nums, visited)
+        return res
+    
+    def dfs(self, res, tmp, nums, visited):
+        if len(visited) == len(nums):
+            res.append(tmp.copy())
+            return
+        start = set([])
+        for i in range(len(nums)):
+            if i in visited:
+                continue
+            if nums[i] in start:
+                continue
+            start.add(nums[i])
+            
+            tmp.append(nums[i])
+            visited.add(i)
+            self.dfs(res, tmp, nums, visited)
+            visited.remove(i)
+            tmp.pop()
+            
