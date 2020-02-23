2701edae6b81df4aa4e0c1c73e3e69ba5237680f
widen <yuangao719@gmail.com>
2020-02-23 18:57:51 -0500

    LC329 revisit

--- Widen/LC329_Longest_Increasing_Path_in_a_Matrix.py
+++ Widen/LC329_Longest_Increasing_Path_in_a_Matrix.py
@@ -26,6 +26,36 @@ Output: 4
 Explanation: The longest increasing path is [3, 4, 5, 6]. Moving diagonally is not allowed.
 """
 
+# revisit.. 
+# Runtime: 516 ms, faster than 59.22% of Python3 online submissions for Longest Increasing Path in a Matrix.
+# Memory Usage: 15.6 MB, less than 30.77% of Python3 online submissions for Longest Increasing Path in a Matrix.
+class Solution:
+    def longestIncreasingPath(self, matrix: List[List[int]]) -> int:
+        if len(matrix) == 0 or len(matrix[0]) == 0:
+            return 0
+        n_row, n_col = len(matrix), len(matrix[0])
+        points = []
+        hash_dict = {}
+        DIRECTIONS = [[-1, 0], [1, 0], [0, -1], [0, 1]]
+        for i in range(n_row):
+            for j in range(n_col):
+                points.append((matrix[i][j], i, j))
+                
+        points = sorted(points, key=lambda x:x[0], reverse=True)
+        for i in range(len(points)):
+            x, y = points[i][1], points[i][2]
+            hash_dict[(x, y)] = 1
+            for dx, dy in DIRECTIONS:
+                new_x, new_y = x + dx, y + dy 
+                if new_x < 0 or new_x >= n_row or new_y < 0 or new_y >= n_col:
+                    continue 
+                if (new_x, new_y) in hash_dict and matrix[x][y] < matrix[new_x][new_y]:
+                    hash_dict[(x, y)] = max(hash_dict[(x, y)], hash_dict[(new_x, new_y)]+1)
+                    
+        return max(hash_dict.values())
+
+# Runtime: 648 ms, faster than 20.62% of Python3 online submissions for Longest Increasing Path in a Matrix.
+# Memory Usage: 15.3 MB, less than 38.46% of Python3 online submissions for Longest Increasing Path in a Matrix.
 # met this problem in an interview... But clearly I failed...
 # just dp and dfs, nothing special...don't know why I failed...
 class Solution:
