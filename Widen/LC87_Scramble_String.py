f04453fc61fdf3b05f01b4db77f8510bcacea6f4
widen <yuangao719@gmail.com>
2020-02-23 19:22:44 -0500

    LC87

---
+++ Widen/LC87_Scramble_String.py
@@ -0,0 +1,73 @@
+"""
+87. Scramble String
+Given a string s1, we may represent it as a binary tree by partitioning it to two non-empty substrings recursively.
+
+Below is one possible representation of s1 = "great":
+
+    great
+   /    \
+  gr    eat
+ / \    /  \
+g   r  e   at
+           / \
+          a   t
+To scramble the string, we may choose any non-leaf node and swap its two children.
+
+For example, if we choose the node "gr" and swap its two children, it produces a scrambled string "rgeat".
+
+    rgeat
+   /    \
+  rg    eat
+ / \    /  \
+r   g  e   at
+           / \
+          a   t
+We say that "rgeat" is a scrambled string of "great".
+
+Similarly, if we continue to swap the children of nodes "eat" and "at", it produces a scrambled string "rgtae".
+
+    rgtae
+   /    \
+  rg    tae
+ / \    /  \
+r   g  ta  e
+       / \
+      t   a
+We say that "rgtae" is a scrambled string of "great".
+
+Given two strings s1 and s2 of the same length, determine if s2 is a scrambled string of s1.
+
+Example 1:
+
+Input: s1 = "great", s2 = "rgeat"
+Output: true
+Example 2:
+
+Input: s1 = "abcde", s2 = "caebd"
+Output: false
+"""
+
+
+
+# I usually amazed by the intelligence of human
+# tutorial: https://blog.csdn.net/fuxuemingzhu/article/details/84950326
+# Runtime: 32 ms, faster than 94.46% of Python3 online submissions for Scramble String.
+# Memory Usage: 12.8 MB, less than 100.00% of Python3 online submissions for Scramble String.
+class Solution:
+    def isScramble(self, s1: str, s2: str) -> bool:
+        if len(s1) != len(s2):
+            return False 
+        
+        n = len(s1)
+        if n == 0:
+            return True 
+        if n == 1:
+            return s1 == s2
+        if sorted(s1) != sorted(s2):
+            return False
+        for i in range(1, n):
+            if self.isScramble(s1[:i], s2[:i]) and self.isScramble(s1[i:], s2[i:]):
+                return True 
+            if self.isScramble(s1[:i], s2[-i:]) and self.isScramble(s1[i:], s2[:-i]):
+                return True 
+        return False 
