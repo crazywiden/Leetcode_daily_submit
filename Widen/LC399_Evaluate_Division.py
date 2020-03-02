26cbd3aebc53a46a6227712398a1e933f70f684f
widen <yuangao719@gmail.com>
2020-03-02 14:12:24 -0500

    revisited

--- Widen/LC399_Evaluate_Division.py
+++ Widen/LC399_Evaluate_Division.py
@@ -21,6 +21,8 @@ queries = [ ["a", "c"], ["b", "a"], ["a", "e"], ["a", "a"], ["x", "x"] ].
 # best part of this problem is to transform this problem to a graph problem
 # use Union Find -- also create a dictionary to perform union and find, which is genius
 # reference: https://zxi.mytechroad.com/blog/graph/leetcode-399-evaluate-division/
+# Runtime: 20 ms, faster than 98.04% of Python3 online submissions for Evaluate Division.
+# Memory Usage: 12.7 MB, less than 100.00% of Python3 online submissions for Evaluate Division.
 class Solution:
     def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
         def find(node):
@@ -56,3 +58,39 @@ class Solution:
         res = [divide(x, y) if x in UnionFindDict and y in UnionFindDict else -1 for x, y in queries]
         return res
 
+
+# dfs solution
+# Runtime: 24 ms, faster than 89.16% of Python3 online submissions for Evaluate Division.
+# Memory Usage: 12.9 MB, less than 100.00% of Python3 online submissions for Evaluate Division.
+class Solution:
+    def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
+        graph = collections.defaultdict(dict)
+        for i in range(len(equations)):
+            node1, node2 = equations[i]
+            graph[node1][node2] = values[i]
+            graph[node2][node1] = 1.0 / values[i]
+        n = len(queries)
+        res = [0 for _ in range(n)]
+        for i in range(n):
+            node1, node2 = queries[i]
+            res[i] = self.dfs(graph, node1, node2, set([node1]), 1)
+        return res 
+    
+    def dfs(self, graph, node1, node2, visited, res):
+        if node1 not in graph:
+            return -1 
+        if node2 in graph[node1]:
+            return res * graph[node1][node2]
+        flag = False
+        for node in graph[node1]:
+            if node in visited:
+                continue
+            visited.add(node)
+            tmp = self.dfs(graph, node, node2, visited, res*graph[node1][node])
+            if tmp != -1:
+                flag = True
+                res = tmp
+        if not flag:
+            return -1
+        return res
+            
