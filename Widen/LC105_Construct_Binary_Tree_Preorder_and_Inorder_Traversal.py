508e3bb3cf0b971ac0d092f378d98f215087ffe1
widen <yuangao719@gmail.com>
2020-03-21 20:51:51 -0400

    revisted LC105

--- Widen/LC105_Construct_Binary_Tree_Preorder_and_Inorder_Traversal.py
+++ Widen/LC105_Construct_Binary_Tree_Preorder_and_Inorder_Traversal.py
@@ -22,7 +22,94 @@ Return the following binary tree:
 this problem is classic and thus relatively easy
 However, the challenge is how to speed up
 """
+# optimized using hashmap
+# Runtime: 56 ms, faster than 89.74% of Python3 online submissions for Construct Binary Tree from Preorder and Inorder Traversal.
+# Memory Usage: 17.8 MB, less than 86.84% of Python3 online submissions for Construct Binary Tree from Preorder and Inorder Traversal.
+class Solution:
+    def buildTree(self, preorder: List[int], inorder: List[int]) -> TreeNode:
+        self.inorder_idx = {val:idx for idx, val in enumerate(inorder)}
+        n = len(inorder)
+        return self.helper(preorder, 0, n, inorder, 0, n)
 
+    def helper(self, preorder, pre_start, pre_end, inorder, in_start, in_end):
+        if pre_start >= pre_end or in_start >= in_end:
+            return None
+        head = TreeNode(preorder[pre_start])
+        root_idx = self.inorder_idx[preorder[pre_start]]
+        left_len = root_idx - in_start
+        right_len = in_end - root_idx
+        
+        left_tree = self.helper(preorder, pre_start+1, pre_start+1+left_len, inorder, in_start, root_idx)
+        right_tree = self.helper(preorder, pre_start+1+left_len, pre_end, inorder, root_idx+1, in_end)
+        
+        head.left = left_tree
+        head.right = right_tree
+        return head
+    
+# used the number of element in each tree to separate left tree and right tree
+# Runtime: 444 ms, faster than 8.37% of Python3 online submissions for Construct Binary Tree from Preorder and Inorder Traversal.
+# Memory Usage: 88.3 MB, less than 13.16% of Python3 online submissions for Construct Binary Tree from Preorder and Inorder Traversal.
+class Solution:
+    def buildTree(self, preorder: List[int], inorder: List[int]) -> TreeNode:
+        # can also make use of the number of elements
+        if len(preorder) == 0:
+            return None
+        root = TreeNode(preorder[0])
+        for i in range(len(inorder)):
+            if inorder[i] == preorder[0]:
+                root_idx = i
+                break
+        num_left = root_idx
+        num_right = len(inorder) - root_idx
+        left_inorder, right_inorder = inorder[:root_idx], inorder[root_idx+1:]
+        left_preorder, right_preorder = preorder[1:1+root_idx], preorder[root_idx+1:]
+        left_tree = self.buildTree(left_preorder, left_inorder)
+        right_tree = self.buildTree(right_preorder, right_inorder)
+        root.left = left_tree
+        root.right = right_tree
+        return root
+    
+
+            
+        
+# used hash map to separate the left tree and right tree in pre-order tranversal
+# Runtime: 456 ms, faster than 7.81% of Python3 online submissions for Construct Binary Tree from Preorder and Inorder Traversal.
+# Memory Usage: 88 MB, less than 13.16% of Python3 online submissions for Construct Binary Tree from Preorder and Inorder Traversal.
+class Solution:
+    def buildTree(self, preorder: List[int], inorder: List[int]) -> TreeNode:
+        if len(preorder) == 0:
+            return None
+        root = TreeNode(preorder[0])
+        for i in range(len(inorder)):
+            if inorder[i] == preorder[0]:
+                root_idx = i
+                break
+    
+        if root_idx == 0:
+            left_tree = None
+            right_preorder = preorder[1:] 
+            right_inorder = inorder[1:]
+            right_tree = self.buildTree(right_preorder, right_inorder) 
+        elif root_idx == len(inorder) - 1:
+            right_tree = None
+            left_preorder = preorder[1:]
+            left_inorder = inorder[:-1]
+            left_tree = self.buildTree(left_preorder, left_inorder)
+        else:
+            left_inorder, right_inorder = inorder[:root_idx], inorder[root_idx+1:]
+            left_ele = set(left_inorder)
+            for i in range(1, len(preorder)):
+                if preorder[i] not in left_ele:
+                    left_preorder, right_preorder = preorder[1:i], preorder[i:]
+                    break
+
+            left_tree = self.buildTree(left_preorder, left_inorder)
+            right_tree = self.buildTree(right_preorder, right_inorder)    
+        
+        root.left = left_tree
+        root.right = right_tree
+        return root
+        
 # method 1 -- just normal recursion
 # Runtime: 216 ms, faster than 22.87% of Python3 online submissions for Construct Binary Tree from Preorder and Inorder Traversal.
 # Memory Usage: 88.6 MB, less than 13.16% of Python3 online submissions for Construct Binary Tree from Preorder and Inorder Traversal.
