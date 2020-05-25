a6fbb4ba9d3bca7be903123023c9a005e7d70367
widen <yuangao719@gmail.com>
2020-05-24 22:06:05 -0400

    LC740_py

--- Widen/LC740_delete_and_earn.py
+++ Widen/LC740_delete_and_earn.py
@@ -87,7 +87,79 @@ class Solution:
                 prevprev,prev = prev,prev+num*counter[num]
         return prev
             
+# revisit
+# Runtime: 52 ms, faster than 89.05% of Python3 online submissions for Delete and Earn.
+# Memory Usage: 14 MB, less than 25.00% of Python3 online submissions for Delete and Earn.
+class Solution:
+    def deleteAndEarn(self, nums: List[int]) -> int:
+        if len(nums) == 0:
+            return 0
+        freq = collections.Counter(nums)
+        freq = {key: key * freq[key] for key in freq.keys()}
+        
+        prev, curr = 0, 0
+        for key in sorted(freq.keys()):
+            if key - 1 in freq:
+                curr, prev = max(curr, prev + freq[key]), curr
+            else:
+                prev = curr
+                curr = freq[key] + curr
+        return max(curr, prev)
+# try to backtracking but TLE
+import collections
+class Solution:
+    def deleteAndEarn(self, nums: List[int]) -> int:
+        if len(nums) == 0:
+            return 0
+        freq = collections.Counter(nums)
+        freq_sum = {key:key*freq[key] for key in freq.keys()}
+        
+        max_int = max(freq.keys())
+        # if visited, status[key] = 1
+        # if not visited, status[key] = 0
+        self.status = set(["1"*(max_int+1)])
+        res = 0
+        return self.dfs(freq_sum, res, "0" * (max_int+1))
+        
+    def dfs(self, freq_sum, res, curr_status):
+        if curr_status in self.status:
+            return res
+        curr = 0
+        self.status.add(curr_status)
+        all_key = list(freq_sum.keys())
+        for key in all_key:
+            if curr_status[key] == "1":
+                continue
+                
+            curr_val = [freq_sum.pop(key), True]
+            prev_val = [-1, False]
+            next_val = [-1, False]
+            curr_status = curr_status[:key] + "1" + curr_status[key+1:]
+            if key + 1 in freq_sum:
+                curr_status = curr_status[:key+1] + "1" + curr_status[key+2:]
+                next_val = [freq_sum.pop(key+1), True]
+            if key - 1 in freq_sum:
+                curr_status = curr_status[:key-1] + "1" + curr_status[key:]
+                prev_val = [freq_sum.pop(key-1), True]
+                
             
+            tmp = self.dfs(freq_sum, curr_val[0], curr_status)
+            curr = max(curr, tmp)
+            
+            curr_status = curr_status[:key] + "0" + curr_status[key+1:]
+            freq_sum[key] = curr_val[0]
+            
+            if next_val[1]:
+                curr_status = curr_status[:key+1] + "0" + curr_status[key+2:]
+                freq_sum[key+1] = next_val[0]
+            if prev_val[1]:
+                curr_status = curr_status[:key-1] + "0" + curr_status[key:]
+                freq_sum[key-1] = prev_val[0]
+
+        return res + curr
+        
+        
+
             
         
         
