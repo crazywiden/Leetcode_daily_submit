b831e8c50bdee57cb1a823c2bb8bad50414d3421
widen <yuangao719@gmail.com>
2020-08-05 02:19:35 -0700

    高远靠得住

--- Widen/LC300_Longest_Increasing_Subsequence.py
+++ Widen/LC300_Longest_Increasing_Subsequence.py
@@ -16,6 +16,20 @@ Your algorithm should run in O(n2) complexity.
 Follow up: Could you improve it to O(n log n) time complexity?
 """
 
+# dp
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        if len(nums) == 0:
+            return 0
+        N = len(nums)
+        dp = [1 for _ in range(N)]
+        for i in range(1, N):
+            for j in range(i-1, -1, -1):
+                if nums[j] >= nums[i]:
+                    continue
+                dp[i] = max(dp[i], dp[j] + 1)
+        return max(dp)
+    
 # credit to -->  https://www.cnblogs.com/grandyang/p/4938187.html
 
 # solutoin 1 -- dp
