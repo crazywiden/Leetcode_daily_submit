5d8b5c9056676be796d517207d3ec78ba126ddea
widen <yuangao719@gmail.com>
2020-09-04 23:16:53 -0700

    LC47 revisit

--- Widen/LC47_PermutationS_II.py
+++ Widen/LC47_PermutationS_II.py
@@ -13,6 +13,35 @@ Output:
 ]
 """
 
+# revisit
+# Runtime: 60 ms, faster than 72.26% of Python3 online submissions for Permutations II.
+# Memory Usage: 14 MB, less than 68.00% of Python3 online submissions for Permutations II.
+class Solution:
+    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+        if len(nums) <= 1:
+            return [nums]
+        nums = sorted(nums)
+        res = []
+        for i in range(len(nums)):
+            if i > 0 and nums[i] == nums[i-1]:
+                continue
+            tmp_nums = nums[:i] + nums[i+1:]
+            for tmp_res in self.helper(tmp_nums):
+                res.append([nums[i]] + tmp_res)
+        return res
+    
+    def helper(self, nums):
+        if len(nums) <= 1:
+            return [nums]
+        res = []
+        for i in range(len(nums)):
+            if i > 0 and nums[i] == nums[i-1]:
+                continue
+            tmp_nums = nums[:i] + nums[i+1:]
+            for tmp_res in self.helper(tmp_nums):
+                res.append([nums[i]] + tmp_res)
+        return res
+
 
 # Runtime: 48 ms, faster than 93.42% of Python3 online submissions for Permutations II.
 # Memory Usage: 12.8 MB, less than 100.00% of Python3 online submissions for Permutations II.
