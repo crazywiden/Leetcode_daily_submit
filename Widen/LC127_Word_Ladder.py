08918dc34b7d61da8cc9f17702a749a972a6a78d
widen <yuangao719@gmail.com>
2020-09-07 00:38:40 -0700

    LC127

--- Widen/LC127_Word_Ladder.py
+++ Widen/LC127_Word_Ladder.py
@@ -1,3 +1,126 @@
+"""
+127. Word Ladder
+Given two words (beginWord and endWord), and a dictionary's word list, find the length of shortest transformation sequence from beginWord to endWord, such that:
+
+Only one letter can be changed at a time.
+Each transformed word must exist in the word list.
+Note:
+
+Return 0 if there is no such transformation sequence.
+All words have the same length.
+All words contain only lowercase alphabetic characters.
+You may assume no duplicates in the word list.
+You may assume beginWord and endWord are non-empty and are not the same.
+Example 1:
+
+Input:
+beginWord = "hit",
+endWord = "cog",
+wordList = ["hot","dot","dog","lot","log","cog"]
+
+Output: 5
+
+Explanation: As one shortest transformation is "hit" -> "hot" -> "dot" -> "dog" -> "cog",
+return its length 5.
+Example 2:
+
+Input:
+beginWord = "hit"
+endWord = "cog"
+wordList = ["hot","dot","dog","lot","log"]
+
+Output: 0
+
+Explanation: The endWord "cog" is not in wordList, therefore no possible transformation.
+"""
+# better solution to represent the one-at-a-time relationship
+# Runtime: 112 ms, faster than 93.32% of Python3 online submissions for Word Ladder.
+# Memory Usage: 17.2 MB, less than 50.40% of Python3 online submissions for Word Ladder.
+
+from collections import defaultdict
+class Solution(object):
+    def ladderLength(self, beginWord, endWord, wordList):
+        """
+        :type beginWord: str
+        :type endWord: str
+        :type wordList: List[str]
+        :rtype: int
+        """
+
+        if endWord not in wordList or not endWord or not beginWord or not wordList:
+            return 0
+
+        # Since all words are of same length.
+        L = len(beginWord)
+
+        # Dictionary to hold combination of words that can be formed,
+        # from any given word. By changing one letter at a time.
+        all_combo_dict = defaultdict(list)
+        for word in wordList:
+            for i in range(L):
+                # Key is the generic word
+                # Value is a list of words which have the same intermediate generic word.
+                all_combo_dict[word[:i] + "*" + word[i+1:]].append(word)
+
+
+        # Queue for BFS
+        queue = collections.deque([(beginWord, 1)])
+        # Visited to make sure we don't repeat processing same word.
+        visited = {beginWord: True}
+        while queue:
+            current_word, level = queue.popleft()      
+            for i in range(L):
+                # Intermediate words for current word
+                intermediate_word = current_word[:i] + "*" + current_word[i+1:]
+
+                # Next states are all the words which share the same intermediate state.
+                for word in all_combo_dict[intermediate_word]:
+                    # If at any point if we find what we are looking for
+                    # i.e. the end word - we can return with the answer.
+                    if word == endWord:
+                        return level + 1
+                    # Otherwise, add it to the BFS Queue. Also mark it visited
+                    if word not in visited:
+                        visited[word] = True
+                        queue.append((word, level + 1))
+                all_combo_dict[intermediate_word] = []
+        return 0
+
+# bfs solution
+# time complexity -- O(N^2)
+# TLE
+
+class Solution:
+    def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
+        tot_word = wordList + [beginWord]
+        N = len(tot_word)
+        
+        queue = [[N-1, 1]]
+        visited = set([N-1])
+        while queue:
+            node, step = queue.pop(0)
+            if tot_word[node] == endWord:
+                return step
+            for i in range(N):
+                if i in visited:
+                    continue
+                if self.is_changable(tot_word[i], tot_word[node]):
+                    queue.append([i, step+1])
+                    visited.add(i)
+        return 0
+        
+    def is_changable(self, word1, word2):
+        change_cnt = 0
+        for i, j in zip(word1, word2):
+            if i != j:
+                change_cnt += 1
+            if change_cnt > 1:
+                return False
+        return True
+    
+        
+
+
 from collections import defaultdict
 class Solution(object):
     def ladderLength(self, beginWord, endWord, wordList):
